f0604471331f5ab29ffcfa31f415f6d8fded52f1
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-02-10 14:55:10 -0600

    Added Deletion Method  - This method Searches a specific element from the 2D array and Delete that value.

--- src/com/company/Data_Structure/Big0/Arrays/ShuffleArray.java
+++ src/com/company/Data_Structure/Big0/Arrays/ShuffleArray.java
@@ -1,5 +1,6 @@
 package com.company.Data_Structure.Big0.Arrays;
 
+import javax.print.attribute.PrintJobAttributeSet;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -10,7 +11,7 @@ public class ShuffleArray {
 
         int nums[] = {1,2,3,4,4,3,2,1};
         System.out.println(nums[nums.length/2]);
-        int [] result = shuffleArrays(nums, nums.length/2);
+        int [] result = shuffleArrays(nums, nums.length);
 
         System.out.println(Arrays.toString(result));
 
@@ -20,40 +21,20 @@ public class ShuffleArray {
 
     public static int [] shuffleArrays(int [] nums, int n){
 
-
-
-        for(int i = 0; i< n; i++){
-
-        }
-
-
-
-
-
-
-
-        int j = n;
         int i = 1;
+        int mid = n/2;
 
-        int length = nums.length;
 
-        while( i< j){
-            do{
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        while(i < mid && mid < nums.length){
 
-                i++;
-                j++;
-            }while (i < n && n < length);
+            int temp = nums[i];
+            nums[i] = nums[mid];
+            nums[mid] = temp;
+
 
             i++;
-
-
-
+            mid++;
         }
-
-
 
 
 
---
+++ src/com/company/Data_Structure/Big0/Arrays/numIdenticalPairs.java
@@ -0,0 +1,38 @@
+package com.company.Data_Structure.Big0.Arrays;
+
+public class numIdenticalPairs {
+
+    public static void main(String[] args) {
+
+
+        int []nums = {1,1,1,1};
+
+        System.out.println(numIdenticalPairs(nums));
+
+    }
+
+    public static int numIdenticalPairs(int[] nums) {
+
+        int count = 0;
+
+
+
+        for( int i = 0; i< nums.length; i++){
+            int j = i + 1;
+
+            while( j < nums.length) {
+                if (nums[i] == nums[j]) {
+                    count += 1;
+
+                }
+                j++;
+            }
+
+        }
+
+
+
+
+        return count;
+    }
+}
