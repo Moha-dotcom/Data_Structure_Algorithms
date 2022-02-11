4f28bf121f2b51206c170eb5712c901c784f2644
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-02-10 19:47:06 -0600

    You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
    You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.

---
+++ src/com/company/Data_Structure/Big0/Arrays/isUnique.java
@@ -0,0 +1,24 @@
+package com.company.Data_Structure.Big0.Arrays;
+
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
+
+public class isUnique {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(isUnique(new int[]{1,2,3,4}));
+
+    }
+
+
+    public static boolean isUnique(int [] arr){
+
+
+
+
+        return true;
+    }
+}
